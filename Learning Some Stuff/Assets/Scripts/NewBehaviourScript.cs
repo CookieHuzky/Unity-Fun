c5d43e6c6acd35fa84ed3bed63220988db7a7585
QueasyRat274365 <59179140+QueasyRat274365@users.noreply.github.com>
2021-03-12 16:05:14 +0100

    Create NewBehaviourScript.cs

---
+++ Learning Some Stuff/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    int frames = 0;
+    void Start()
+    {
+        while (Input.GetKeyDown(KeyCode.E))
+        {
+            frames++;
+            Debug.Log("E " + frames + " Frames lang gedrückt");
+        }
+    }
+}
