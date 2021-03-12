56d7e5bd5d45406f1d9caf44bc67f5c7fd2e2118
QueasyRat274365 <59179140+QueasyRat274365@users.noreply.github.com>
2021-03-12 16:08:45 +0100

    Removed Start function

--- Learning Some Stuff/Assets/Scripts/PauseMenu.cs
+++ Learning Some Stuff/Assets/Scripts/PauseMenu.cs
@@ -7,13 +7,6 @@ public class PauseMenu : MonoBehaviour
 {
     public static bool GamePaused = false;
     public GameObject pauseMenu;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
