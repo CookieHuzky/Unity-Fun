62c8d53b6f16243b2c688ea2048c59c28f0245c1
Cookie <59179140+QueasyRat274365@users.noreply.github.com>
2021-03-26 14:35:51 +0100

    Create BeanMovement.cs

---
+++ Learning Some Stuff/Assets/Scripts/BeanMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeanMovement : MonoBehaviour
+{
+    private CapsuleCollider c;
+    private Rigidbody rb;
+    void Start()
+    {
+        c = gameObject.GetComponent<CapsuleCollider>();
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Jump()
+    {
+            rb.velocity = Vector3.up * 10;
+    }
+    void Update()
+    {
+        if (IsGrounded() && Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+    private bool IsGrounded()
+    {
+        float extraHeight = .01f;
+        bool rayHit = Physics.Raycast(c.bounds.center, Vector3.down, c.bounds.extents.y + extraHeight);
+        Color rayColor;
+        if (rayHit)
+        {
+            rayColor = Color.green;
+        }
+        else
+        {
+            rayColor = Color.red;
+        }
+        Debug.DrawRay(c.bounds.center, Vector3.down * (c.bounds.extents.y + extraHeight));
+        return rayHit != false;
+    }
+}
